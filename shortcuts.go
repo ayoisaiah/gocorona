d3884da915598790860ba927a0e5d6f38527225c
Ayooluwa Isaiah <ayisaiah@gmail.com>
2020-04-10 20:50:05 +0100

    Update table shortcuts

--- shortcuts.go
+++ shortcuts.go
@@ -8,8 +8,8 @@ import (
 
 func shortcuts() *widgets.Paragraph {
 	widget := widgets.NewParagraph()
-	widget.Title = "Keyboard shortcuts"
-	widget.Text = fmt.Sprintf("F1: [Help](fg:black,bg:green) F2: [Sort](fg:black,bg:green)")
+	widget.Title = "Sorting options"
+	widget.Text = fmt.Sprintf("F1 (default): [Cases](fg:black,bg:green) | F2: [Cases Today](fg:black,bg:green) | F3: [Deaths](fg:black,bg:green) | F4: [Deaths Today](fg:black,bg:green) | F5: [Recoveries](fg:black,bg:green) | F6: [Active](fg:black,bg:green) | F7: [Critical](fg:black,bg:green) | F8: [Mortality](fg:black,bg:green)")
 
 	return widget
 }
