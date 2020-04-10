234d6d505a8c941493b04b45b2459f019abfbb3c
Ayooluwa Isaiah <ayisaiah@gmail.com>
2020-04-10 14:44:52 +0100

    Update countries table
    Add mortality rate for each country

--- countries.go
+++ countries.go
@@ -23,33 +23,28 @@ func affectedCountriesTable() (*widgets.Table, error) {
 
 	table := widgets.NewTable()
 	table.Rows = [][]string{
-		[]string{"#", "Country", "Cases", "Cases (today)", "Deaths", "Deaths (today)", "Recoveries", "Active", "Critical"},
+		[]string{"#", "Country", "Cases (today)", "Deaths (today)", "Recoveries", "Active", "Critical", "Mortality"},
 	}
 
 	for i, v := range *countryTotals {
 		table.Rows = append(table.Rows, []string{
 			p.Sprintf("%d", i+1),
 			v.Country,
-			p.Sprintf("%d", v.Cases),
-			p.Sprintf("%d", v.TodayCases),
-			p.Sprintf("%d", v.Deaths),
-			p.Sprintf("%d", v.TodayDeaths),
+			p.Sprintf("%d (%d)", v.Cases, v.TodayCases),
+			p.Sprintf("%d (%d)", v.Deaths, v.TodayDeaths),
 			p.Sprintf("%d", v.Recovered),
 			p.Sprintf("%d", v.Active),
 			p.Sprintf("%d", v.Critical),
+			p.Sprintf("%.2f%s", float64(v.Deaths)/float64(v.Cases)*100, "%"),
 		})
-
-		if i%2 == 0 {
-			table.RowStyles[i] = ui.NewStyle(ui.ColorBlack, ui.ColorYellow)
-		}
 	}
 
-	table.ColumnWidths = []int{5, 20, 15, 15, 15, 15, 15, 15, 15}
+	table.ColumnWidths = []int{5, 20, 20, 20, 15, 15, 13, 13, 14}
 	table.TextAlignment = ui.AlignCenter
 	table.TextStyle = ui.NewStyle(ui.ColorWhite)
 	table.FillRow = true
 	table.RowStyles[0] = ui.NewStyle(ui.ColorWhite, ui.ColorBlack, ui.ModifierBold)
-	table.SetRect(0, 11, 140, 50)
+	table.SetRect(0, 11, 150, 50)
 
 	return table, nil
 }
