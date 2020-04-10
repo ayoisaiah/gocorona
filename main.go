299bf10b788b21bcc35887a745006b41898ddda1
Ayooluwa Isaiah <ayisaiah@gmail.com>
2020-04-10 14:48:11 +0100

    Use grid layout and update UI every 16ms

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	ui "github.com/gizak/termui/v3"
 )
@@ -11,6 +12,10 @@ func main() {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
 	defer ui.Close()
+	tw, th := ui.TerminalDimensions()
+
+	grid := ui.NewGrid()
+	grid.SetRect(0, 0, tw, th)
 
 	globalStats, err := globalStatsWidget()
 	if err != nil {
@@ -22,15 +27,32 @@ func main() {
 		log.Fatal(err)
 	}
 
-	ui.Render(globalStats, countriesTable)
+	gs := ui.NewRow(0.25, ui.NewCol(1.0, globalStats))
+	ct := ui.NewRow(0.67, ui.NewCol(1.0, countriesTable))
+	st := ui.NewRow(0.08, ui.NewCol(1.0, shortcuts()))
 
+	grid.Set(gs, ct, st)
+	ui.Render(grid)
+
+	ticker := time.Tick(time.Second / time.Duration(60))
 	uiEvents := ui.PollEvents()
 	for {
-		e := <-uiEvents
-		switch e.ID {
-		case "q", "<C-c>":
-			return
+		select {
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				return
+			case "j", "<Down>":
+				countriesTable.ScrollDown()
+			case "k", "<Up>":
+				countriesTable.ScrollUp()
+			case "<Resize>":
+				tw, th = ui.TerminalDimensions()
+				grid.SetRect(0, 0, tw, th)
+				ui.Render(grid)
+			}
+		case <-ticker:
+			ui.Render(grid)
 		}
 	}
-
 }
