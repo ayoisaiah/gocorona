c40c310edd10013471e58779c7b2e9ed7907b971
Ayooluwa Isaiah <ayisaiah@gmail.com>
2020-04-12 23:06:32 +0100

    Add emoji to widget title

--- sort.go
+++ sort.go
@@ -13,7 +13,7 @@ type SortOptions struct {
 
 func (self *SortOptions) Construct() {
 	widget := widgets.NewParagraph()
-	widget.Title = "Sorting options"
+	widget.Title = "📈 Sorting options"
 	widget.Text = fmt.Sprintf("F1 (default): [Total Cases](fg:black,bg:green) | F2: [Cases Today](fg:black,bg:green) | F3: [Total Deaths](fg:black,bg:green) | F4: [Deaths Today](fg:black,bg:green) | F5: [Recoveries](fg:black,bg:green) | F6: [Active](fg:black,bg:green) | F7: [Critical](fg:black,bg:green) | F8: [Mortality](fg:black,bg:green)")
 
 	self.Widget = widget
